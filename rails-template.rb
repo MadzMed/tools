37052612ade7304da13a03097d7282aed43f0147
mahdi Lamriben <mehdilmbn@gmail.com>
2020-07-02 18:17:27 +0200

    rails template devise + rspec/2

--- rails-template.rb
+++ rails-template.rb
@@ -90,6 +90,7 @@ end
 ########################################
 markdown_file_content = <<-MARKDOWN
 Rails app generated with [MadzMed/tools/rails-template](https://github.com/MadzMed/tools), based on "Le wagon" template
+use it with [https://raw.githubusercontent.com/MadzMed/tools/master/rails-template.rb]
 MARKDOWN
 file 'README.md', markdown_file_content, force: true
 
